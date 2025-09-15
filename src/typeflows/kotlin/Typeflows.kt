75cf3a249eb02c31922d7918749d25c005a38a47
David Denton <denton.david@gmail.com>
2025-09-15 20:07:05 +0100

    Remove overrides

--- src/typeflows/kotlin/Typeflows.kt
+++ src/typeflows/kotlin/Typeflows.kt
@@ -1,6 +1,4 @@
 import io.typeflows.TypeflowsRepo
-import io.typeflows.env.Env.Companion.javaVersion
-import io.typeflows.fs.TypeflowsResources
 import io.typeflows.github.TypeflowsGitHubRepo
 import io.typeflows.standards.TypeflowsProjectStandards
 import io.typeflows.util.Builder
@@ -8,7 +6,5 @@ import io.typeflows.util.Builder
 class Typeflows : Builder<TypeflowsRepo> {
     override fun build() = TypeflowsGitHubRepo {
         files += TypeflowsProjectStandards()
-        files += TypeflowsResources(Typeflows::class.java)
-        javaVersion = javaVersion("21")
     }
 }
